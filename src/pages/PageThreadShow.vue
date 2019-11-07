6edf37f73f20de9c8bb9aa62fded66315461940f
Kenneth Fernandez <kenneth@mesasix.com>
2019-11-08 01:14:32 +0800

    Add addPost method

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,6 +2,23 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostList :posts="posts" />
+    <form @submit.prevent="addPost">
+      <div>
+        <div class="form-group">
+          <textarea name=""
+                    id=""
+                    cols="30"
+                    rows="10"
+                    class="form-input"
+                    v-model="newPostText"></textarea>
+        </div>
+        <div class="form-actions">
+          <button class="btn-blue">
+            Submit post
+          </button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 <script>
@@ -19,7 +36,8 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      newPostText: ''
     }
   },
   computed: {
@@ -27,6 +45,29 @@ export default {
       const postIds = Object.values(this.thread.posts)
       return Object.values(sourceData.posts)
         .filter(post => postIds.includes(post['.key']))
+    }
+  },
+  mounted () {
+    console.log(sourceData)
+  },
+  methods: {
+    addPost () {
+      const postId = 'greatPost' + Math.random()
+
+      const post = {
+        '.key': postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2'
+      }
+
+      // Add reactivity on added post
+      this.$set(sourceData.posts, postId, post)
+      this.$set(this.thread.posts, postId, postId)
+      this.$set(sourceData.users[post.userId].posts, postId, postId)
+
+      this.newPostText = ''
     }
   }
 }
