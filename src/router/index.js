e9bef6b272c8f4e079fa8e394a7f74adc4d3f9c8
Kenneth Fernandez <kenneth@mesasix.com>
2019-11-08 21:06:27 +0800

    Add category collections of forum

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/PageHome'
+import Category from '@/pages/PageCategory'
 import Forum from '@/pages/PageForum'
 import ThreadShow from '@/pages/PageThreadShow'
 import NotFound from '@/pages/PageNotFound'
@@ -13,6 +14,12 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/category/:id',
+      name: 'Category',
+      component: Category,
+      props: true
     },
     {
       path: '/forum/:id',
