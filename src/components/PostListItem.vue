71987bc67c861a35f452d422244f4e9d7785655d
Kenneth Fernandez <kenneth@mesasix.com>
2019-11-08 15:20:21 +0800

    Add date filters

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -2,21 +2,23 @@
   <div class="post">
     <div class="user-info">
       <a href="profile.html#profile-details"
-         class="user-name">{{ user.name }}</a>
+        class="user-name">{{ user.name }}</a>
       <a href="profile.html#profile-details">
         <img class="avatar-large"
-             :src="user.avatar" />
+          :src="user.avatar" />
       </a>
       <p class="desktop-only text-small">{{ userPostsCount }} posts</p>
     </div>
     <div class="post-content">
       <div>{{ post.text }}</div>
     </div>
-    <div class="post-date text-faded">{{ post.publishedAt }}</div>
+    <div class="post-date text-faded"
+      :title="post.publishedAt | humanFriendlyDate">{{ post.publishedAt | diffForHumans }}</div>
   </div>
 </template>
 <script>
 import sourceData from '@/data'
+import moment from 'moment'
 
 export default {
   props: {
@@ -31,6 +33,14 @@ export default {
     },
     userPostsCount () {
       return Object.keys(this.user.posts).length
+    }
+  },
+  filters: {
+    humanFriendlyDate (date) {
+      return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+    },
+    diffForHumans (date) {
+      return moment.unix(date).fromNow()
     }
   }
 }
