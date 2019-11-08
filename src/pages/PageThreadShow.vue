2e0b05cc112236dc75c60673dbf874a548694bd6
Kenneth Fernandez <kenneth@mesasix.com>
2019-11-08 14:51:53 +0800

    Add PostEditor component and emit event to add new post

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,31 +2,18 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostList :posts="posts" />
-    <form @submit.prevent="addPost">
-      <div>
-        <div class="form-group">
-          <textarea name=""
-                    id=""
-                    cols="30"
-                    rows="10"
-                    class="form-input"
-                    v-model="newPostText"></textarea>
-        </div>
-        <div class="form-actions">
-          <button class="btn-blue">
-            Submit post
-          </button>
-        </div>
-      </div>
-    </form>
+    <PostEditor @save="addPost"
+      :threadId="id" />
   </div>
 </template>
 <script>
 import sourceData from '@/data'
 import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
 export default {
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
   props: {
     id: {
@@ -36,8 +23,7 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id],
-      newPostText: ''
+      thread: sourceData.threads[this.id]
     }
   },
   computed: {
@@ -47,27 +33,15 @@ export default {
         .filter(post => postIds.includes(post['.key']))
     }
   },
-  mounted () {
-    console.log(sourceData)
-  },
   methods: {
-    addPost () {
-      const postId = 'greatPost' + Math.random()
-
-      const post = {
-        '.key': postId,
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now() / 1000),
-        threadId: this.id,
-        userId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2'
-      }
+    addPost (eventData) {
+      const post = eventData.post
+      const postId = eventData.post['.key']
 
       // Add reactivity on added post
       this.$set(sourceData.posts, postId, post)
       this.$set(this.thread.posts, postId, postId)
       this.$set(sourceData.users[post.userId].posts, postId, postId)
-
-      this.newPostText = ''
     }
   }
 }
